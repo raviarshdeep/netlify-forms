b258ae6ad3aaacb7f20e7f6b109f13ffcd98d3d7
Raviarsh <deep.raviarsh@rwit.io>
2024-06-18 17:21:32 +0530

    Added __forms in public folder

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -40,7 +40,7 @@ const Form = () => {
     },
     onSubmit: async (values, { resetForm }) => {
       try {
-        const response = await fetch("/", {
+        const response = await fetch("/__forms.html", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
           body: new URLSearchParams(values).toString(),
