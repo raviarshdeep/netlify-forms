033ae4180e1326d7c8dd605a97fbaf90f4e6bfb2
Raviarsh <deep.raviarsh@rwit.io>
2024-06-18 12:38:21 +0530

    test commit 1

---
+++ src/app/about/page.tsx
@@ -0,0 +1,20 @@
+const About = () => {
+  return (
+    <main>
+     <div>
+     <a
+        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2>
+          About <span>-&gt;</span>
+        </h2>
+        <p>Find in-depth information about Next.js features and API.</p>
+      </a>
+     </div>
+    </main>
+  );
+};
+    
+export default About;
