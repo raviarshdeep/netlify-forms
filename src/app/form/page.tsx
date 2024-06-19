650aca7fa760003d82306f8e2bbfdf8991042726
Raviarsh <deep.raviarsh@rwit.io>
2024-06-19 18:04:42 +0530

    .

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -42,7 +42,7 @@ const Form = () => {
     },
     onSubmit: async (values, { resetForm }) => {
       try {
-        const response = await fetch("/", {
+        const response = await fetch("/contactAS", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
           body: new URLSearchParams(values).toString(),
