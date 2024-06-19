9bde1165447577e401f813c64396f475f81c1b1a
Raviarsh <deep.raviarsh@rwit.io>
2024-06-19 17:01:40 +0530

    Added index files in public folder

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -42,7 +42,7 @@ const Form = () => {
     },
     onSubmit: async (values, { resetForm }) => {
       try {
-        const response = await fetch("/", {
+        const response = await fetch("/__index.html", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
           body: new URLSearchParams(values).toString(),
--- src/app/form2/page.tsx
+++ src/app/form2/page.tsx
@@ -9,51 +9,79 @@ interface FormValues {
 }
 
 const ContactForm = () => {
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-
-    const myForm = event.target;
-
-    const formData = new FormData(myForm);
-    console.log("formData", formData);
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData as any).toString(),
-    })
-      .then(() => alert("Form successfully submitted"))
-      .catch((error) => alert("error"));
-  };
-
   return (
-    <div>
-      <form
-        name="contactT"
-        method="POST"
-        data-netlify="true"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="form-name" value="contactT" />
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </div>
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
+      }}
+      onSubmit={(values, actions) => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: new URLSearchParams(values).toString(),
+        })
+          .then(() => {
+            alert("Success");
+            actions.resetForm();
+          })
+          .catch(() => {
+            alert("Error");
+          });
+      }}
+      validate={(values) => {
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        const errors: Partial<FormValues> = {};
+        if (!values.name) {
+          errors.name = "Name Required";
+        }
+        if (!values.email || !emailRegex.test(values.email)) {
+          errors.email = "Valid Email Required";
+        }
+        if (!values.message) {
+          errors.message = "Message Required";
+        }
+        return errors;
+      }}
+    >
+      {({ handleChange }) => (
+        <form
+          name="application"
+          data-netlify="true"
+          // enctype="multipart/form-data"
+        //   data-netlify-honeypot="bot-field"
+          method="POST"
+        //   noValidate
+        //   action={`/`}
+        >
+          <input type="hidden" name="application" value="application" />
+          {/* <input type="hidden" name="bot-field" onChange={handleChange} /> */}
+
+          <div>
+            <label htmlFor="name">Name: </label>
+            <input type="text" name="name" id="Name" />
+            {/*<ErrorMessage name="name"/>*/}
+          </div>
+
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" id="email" />
+            {/*<ErrorMessage name="email"/>*/}
+          </div>
+
+          <div>
+            <label htmlFor="message">Message: </label>
+            <input type="textarea" name="message" id="message" />
+          </div>
+
+          <button className={"btn btn-primary"} type="submit">
+            Send
+          </button>
+        </form>
+      )}
+    </Formik>
   );
 };
 
