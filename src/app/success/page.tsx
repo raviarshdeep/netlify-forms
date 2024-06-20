10fde0badb6ee33167d52cad0f542d33e9be1e4a
Raviarsh <deep.raviarsh@rwit.io>
2024-06-20 10:43:23 +0530

    Added the contact form

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+// import React from 'react'
 
-const Success = () => {
-  return (
-    <div><h2>Successfully submited data</h2></div>
-  )
+// const Success = () => {
+//   return (
+//     <div><h2>Successfully submited data</h2></div>
+//   )
+// }
+
+// export default Success
+export default function Success() {
+  return <div>Form successfully submitted!</div>
 }
-
-export default Success
