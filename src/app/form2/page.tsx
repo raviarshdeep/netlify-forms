d9da3f02aa367dd622b5aeae8026856b5eac4df7
Raviarsh <deep.raviarsh@rwit.io>
2024-06-20 16:52:52 +0530

    .

--- src/app/form2/page.tsx
+++ src/app/form2/page.tsx
@@ -5,17 +5,17 @@ const ContactForm = () => {
     <form name="contact" method="POST" data-netlify="true">
       <p>
         <label>
-          Your Name: <input type="text" name="nameA" />
+          Your Name: <input type="text" name="nameA" value="ravi"/>
         </label>
       </p>
       <p>
         <label>
-          Your Email: <input type="email" name="emailA" />
+          Your Email: <input type="email" name="emailA" value="ravi@gmail.com"/>
         </label>
       </p>
       <p>
         <label>
-          Message: <textarea name="messageA"></textarea>
+          Message: <textarea name="messageA" value="how are you"></textarea>
         </label>
       </p>
       <p>
