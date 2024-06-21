b7188ea79531bf87d6b028bf94d6bc4af408af27
Raviarsh <deep.raviarsh@rwit.io>
2024-06-21 17:57:43 +0530

    .

--- src/app/form2/page.tsx
+++ src/app/form2/page.tsx
@@ -19,7 +19,7 @@ const MyForm: React.FC = () => {
       const formData = new FormData(myForm);
       console.log("formData",formData)
       // const formData ={}
-      const res = await fetch("/", {
+      const res = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData as any).toString(),
