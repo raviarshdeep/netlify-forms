b2060d505d0f301a23cb56c8c6a0b0710f1d2f42
Raviarsh <deep.raviarsh@rwit.io>
2024-06-21 18:41:30 +0530

    .

--- src/app/form2/page.tsx
+++ src/app/form2/page.tsx
@@ -20,7 +20,7 @@ const Form = () => {
         const response = await fetch("/__forms.html", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", ...values }),
+          body: encode({ "form-name": "feedback", ...values }),
         });
         if (response.ok) {
           alert("Form successfully submitted");
