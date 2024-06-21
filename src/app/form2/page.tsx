f0ead710c0f280e69a86f4c43fe24f3736d542d5
Raviarsh <deep.raviarsh@rwit.io>
2024-06-21 18:10:52 +0530

    .

---
+++ src/app/form2/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+
+const MyForm: React.FC = () => {
+  const encode = (data: any) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+  const handleFormSubmit = async (event: any) => {
+    event.preventDefault();
+    try {
+      const myForm = event.target;
+      console.log("event.target",event.target)
+      const formData = new FormData(myForm);
+      console.log("formData",formData)
+      const res = await fetch("/__forms.html", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData as any).toString(),
+      });
+      if (res.status === 200) {
+        alert("submit");
+      } else {
+        alert("error in form");
+      }
+    } catch (e) {
+      alert("error");
+    }
+  };
+
+  return (
+    <form
+      name="feedback"
+      onSubmit={handleFormSubmit}
+    >
+      <input type="hidden" name="form-name" value="feedback" />
+      <input name="name1" type="text" placeholder="Name" />
+      <input name="email1" type="text" placeholder="Email (optional)"/>
+      <input name="message1" type="text" placeholder="Message" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default MyForm;
